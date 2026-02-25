caa76907fab64c98f12bb2e2a9247225bd7b1d97
lucasemanuel32 <lucasemanuelescola32@gmail.com>
2026-02-25 08:19:47 -0300

    Create exercicio1.js

---
+++ back_end/exercicio1.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>retornar o valor absoluto de um número,</title>
+</head>
+:lucas Eamunel
+<body>
+  <script>
+    var valor1 = Math.abs (-1234);
+    alert (valor1);
+  </script>
+</body>
+</html>
